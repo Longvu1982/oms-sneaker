2a7ba0c982f2b89edbe2bf736b73e7e91eeb997b
Kris.nguyen <tranngochiep200047@gmail.com>
2025-03-24 18:22:11 +0700

    fix(data-table): correct page count calculation in pagination
    The page count calculation was incorrect, leading to inaccurate page navigation. The fix ensures the total number of pages is calculated correctly by dividing the total row count by the page size and rounding up. This improves the accuracy of pagination display and navigation.

--- app/frontend/src/components/data-table/DataTablePagination.tsx
+++ app/frontend/src/components/data-table/DataTablePagination.tsx
@@ -24,10 +24,6 @@ export function DataTablePagination<TData>({
 }: DataTablePaginationProps<TData>) {
   return (
     <div className="flex items-center justify-between px-2">
-      {/* <div className="flex-1 text-sm text-muted-foreground">
-        {table.getFilteredSelectedRowModel().rows.length} of{" "}
-        {table.getFilteredRowModel().rows.length} row(s) selected.
-      </div> */}
       <div className="flex items-center space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
           <p className="text-sm font-medium">Hàng</p>
@@ -50,7 +46,10 @@ export function DataTablePagination<TData>({
           </Select>
         </div>
         <div className="flex max-w-[100px] items-center justify-center text-sm font-medium">
-          {table.getState().pagination.pageIndex + 1} / {table.getPageCount()}
+          {table.getState().pagination.pageIndex + 1} /{" "}
+          {Math.ceil(
+            table.getRowCount() / table.getState().pagination.pageSize
+          )}
         </div>
         <div className="flex items-center space-x-2">
           <Button
