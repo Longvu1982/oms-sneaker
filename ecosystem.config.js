ff03f7d6dcbf63b3ca2da7be83d03867cbd3751a
Long Vu (Kris Nguyen) <89139559+Longvu1982@users.noreply.github.com>
2025-09-07 01:51:14 +0700

    chore: comment out oms-frontend config in ecosystem file
    The frontend is now being served separately, so this configuration is no longer needed in the PM2 ecosystem file.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,24 +16,24 @@ module.exports = {
         PORT: 6060
       }
     },
-    {
-      name: 'oms-frontend',
-      script: 'serve',
-      instances: 1,
-      autorestart: true,
-      watch: ['./app/frontend/dist'],
-      ignore_watch: ['node_modules'],
-      watch_options: {
-        followSymlinks: false
-      },
-      max_memory_restart: '1G',
-      env_production: {
-        NODE_ENV: 'production',
-        PM2_SERVE_PATH: './app/frontend/dist',
-        PM2_SERVE_PORT: 5173,
-        PM2_SERVE_SPA: 'true',
-        PM2_SERVE_HOMEPAGE: '/index.html'
-      }
-    }
+    // {
+    //   name: 'oms-frontend',
+    //   script: 'serve',
+    //   instances: 1,
+    //   autorestart: true,
+    //   watch: ['./app/frontend/dist'],
+    //   ignore_watch: ['node_modules'],
+    //   watch_options: {
+    //     followSymlinks: false
+    //   },
+    //   max_memory_restart: '1G',
+    //   env_production: {
+    //     NODE_ENV: 'production',
+    //     PM2_SERVE_PATH: './app/frontend/dist',
+    //     PM2_SERVE_PORT: 5173,
+    //     PM2_SERVE_SPA: 'true',
+    //     PM2_SERVE_HOMEPAGE: '/index.html'
+    //   }
+    // }
   ]
 };
