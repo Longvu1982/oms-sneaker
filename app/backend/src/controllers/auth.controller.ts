094766c98baa36ff9e387ed75cdb5239d7c39dae
Long Vu (Kris Nguyen) <89139559+Longvu1982@users.noreply.github.com>
2026-09-15 22:09:59 +0700

    feat(nginx): add  server proxy config

--- app/backend/src/controllers/auth.controller.ts
+++ app/backend/src/controllers/auth.controller.ts
@@ -26,7 +26,7 @@ export const login = async (request: Request, response: Response, next: NextFunc
       response.cookie('jwt', token, {
         httpOnly: true,
         // secure: process.env.APP_ENV !== 'developement',
-        secure: false,
+        secure: true,
         sameSite: 'lax',
         maxAge: 30 * 24 * 60 * 60 * 1000,
       });
--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -25,6 +25,7 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
+app.set('trust proxy', 1)
 // CORS Middleware
 const corsOptions = {
   origin: process.env.APP_ENV == 'developement' ? '*' : process.env.ORIGIN,
